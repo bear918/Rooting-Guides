62ed482fa7638b6996d630ada51704d22df09fa8
Remzi Cavdar <remzi_klep@live.nl>
2012-10-06 18:20:02 +0200

    New
    Jaaah

---
+++ gen/root/world/guide/free/remzi/cavdar/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package root.world.guide.free.remzi.cavdar;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/root/world/guide/free/remzi/cavdar/MainActivity.java
@@ -0,0 +1,43 @@
+package root.world.guide.free.remzi.cavdar;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.WindowManager;
+import android.widget.Toast;
+import org.apache.cordova.*;
+
+public class MainActivity extends DroidGap
+{
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        super.loadUrl("file:///android_asset/www/index.html");
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.about: super.loadUrl("file:///android_asset/www/about.html");
+        }
+        switch (item.getItemId()) {
+            case R.id.close: finish ();
+        }
+        switch (item.getItemId()) {
+            case R.id.index: super.loadUrl("file:///android_asset/www/index.html");
+        }
+        switch (item.getItemId()) {
+            case R.id.refresh: Toast.makeText(this, "activity has been restarted", Toast.LENGTH_SHORT).show();
+            break;
+        }
+        return true;
+    }
+}
