8336b967a392d40f86dd48c0570eaa15b1bc6670
Remzi Cavdar <remzi_klep@live.nl>
2012-10-06 18:55:03 +0200

    To latast version

---
+++ gen/root/world/guide/free/remzi/cavdar/open/remzicavdar/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package root.world.guide.free.remzi.cavdar.open.remzicavdar;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
